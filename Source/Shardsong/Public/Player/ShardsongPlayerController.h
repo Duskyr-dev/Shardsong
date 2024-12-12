c58c732342fc678ec304c8a7c811765aeb58b992
Duskkyr <duskyre@outlook.com>
2024-12-11 17:15:40 -0700

    Added Enhanced Input

---
+++ Source/Shardsong/Public/Player/ShardsongPlayerController.h
@@ -0,0 +1,17 @@
+// Copyright Moonblossom Studios
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "ShardsongPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SHARDSONG_API AShardsongPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
