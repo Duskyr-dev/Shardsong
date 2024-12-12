05baa52c8f22dad23fadb8f653e289e01cd1ca57
Duskkyr <duskyre@outlook.com>
2024-12-11 18:06:20 -0700

    Set Up Nyxa Player Controller

--- Source/Shardsong/Private/Player/ShardsongPlayerController.cpp
+++ Source/Shardsong/Private/Player/ShardsongPlayerController.cpp
@@ -2,4 +2,28 @@
 
 
 #include "Player/ShardsongPlayerController.h"
+#include "EnhancedInputSubsystems.h"
 
+AShardsongPlayerController::AShardsongPlayerController()
+{
+	bReplicates = true;
+}
+
+void AShardsongPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+	check(NyxaContext);
+
+	UEnhancedInputLocalPlayerSubsystem * Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer());
+	check(Subsystem);
+	Subsystem->AddMappingContext(NyxaContext, 0);
+
+	bShowMouseCursor = true;
+	DefaultMouseCursor = EMouseCursor::Default;
+
+	FInputModeGameAndUI InputModeData;
+	InputModeData.SetLockMouseToViewportBehavior(EMouseLockMode::DoNotLock);
+	InputModeData.SetHideCursorDuringCapture(false);
+	SetInputMode(InputModeData);
+
+}
--- Source/Shardsong/Public/Player/ShardsongPlayerController.h
+++ Source/Shardsong/Public/Player/ShardsongPlayerController.h
@@ -6,6 +6,9 @@
 #include "GameFramework/PlayerController.h"
 #include "ShardsongPlayerController.generated.h"
 
+
+class UInputMappingContext;
+
 /**
  * 
  */
@@ -14,4 +17,13 @@ class SHARDSONG_API AShardsongPlayerController : public APlayerController
 {
 	GENERATED_BODY()
 	
+public:
+	AShardsongPlayerController();
+
+protected:
+	virtual void BeginPlay() override;
+
+private:
+	UPROPERTY(EditAnywhere, Category = "Input")
+	TObjectPtr<UInputMappingContext> NyxaContext;
 };
