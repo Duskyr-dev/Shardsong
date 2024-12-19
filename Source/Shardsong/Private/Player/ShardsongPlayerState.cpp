2b5a4a9129d025b3576e247ec531c16954f4592d
Duskkyr <duskyre@outlook.com>
2024-12-18 20:57:38 -0700

    Create the PlayerState Class

---
+++ Source/Shardsong/Private/Player/ShardsongPlayerState.cpp
@@ -0,0 +1,9 @@
+// Copyright Moonblossom Studios
+
+
+#include "Player/ShardsongPlayerState.h"
+
+AShardsongPlayerState::AShardsongPlayerState()
+{
+	NetUpdateFrequency = 100.f;
+}
---
+++ Source/Shardsong/Public/Player/ShardsongPlayerState.h
@@ -0,0 +1,19 @@
+// Copyright Moonblossom Studios
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerState.h"
+#include "ShardsongPlayerState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SHARDSONG_API AShardsongPlayerState : public APlayerState
+{
+	GENERATED_BODY()
+	
+public:
+	AShardsongPlayerState();
+};
