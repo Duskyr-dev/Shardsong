aa4d503f24c0a55c8e4982cf5250ba1542f483fc
Duskkyr <duskyre@outlook.com>
2024-12-19 13:31:48 -0700

    Added Health and Mana Attributes

--- Source/Shardsong/Private/AbilitySystem/ShardsongAttributeSet.cpp
+++ Source/Shardsong/Private/AbilitySystem/ShardsongAttributeSet.cpp
@@ -2,4 +2,38 @@
 
 
 #include "AbilitySystem/ShardsongAttributeSet.h"
+#include "AbilitySystemComponent.h"
+#include "Net/UnrealNetwork.h"
 
+UShardsongAttributeSet::UShardsongAttributeSet()
+{
+
+}
+
+void UShardsongAttributeSet::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	DOREPLIFETIME_CONDITION_NOTIFY(UShardsongAttributeSet, Health, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UShardsongAttributeSet, MaxHealth, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UShardsongAttributeSet, Mana, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UShardsongAttributeSet, MaxMana, COND_None, REPNOTIFY_Always);
+}
+
+void UShardsongAttributeSet::OnRep_Health(const FGameplayAttributeData& OldHealth) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UShardsongAttributeSet, Health, OldHealth);
+}
+
+void UShardsongAttributeSet::OnRep_MaxHealth(const FGameplayAttributeData& OldMaxHealth) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UShardsongAttributeSet, MaxHealth, OldMaxHealth);
+}
+
+void UShardsongAttributeSet::OnRep_Mana(const FGameplayAttributeData& OldMana) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UShardsongAttributeSet, Mana, OldMana);
+}
+
+void UShardsongAttributeSet::OnRep_MaxMana(const FGameplayAttributeData& OldMaxMana) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UShardsongAttributeSet, MaxMana, OldMaxMana);
+}
